abc4021c6fab4f974bf4504563aac0f43c9f3fc1
Michael <myorg@yahoo.com>
2018-11-30 00:24:34 -0800

    Refactored currency components as children.

--- src/App.js
+++ src/App.js
@@ -1,54 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Amount from './Amount';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      amount: 0,
-    };
-  }
-
-  onIncrement = () => {
-    this.setState(state => ({ amount: state.amount + 1 }));
-  };
-
-  onDecrement = () => {
-    this.setState(state => ({ amount: state.amount - 1 }));
-  };
-
-  render() {
-    return (
-      <div>
-        <Amount
-          amount={this.state.amount}
-          onIncrement={this.onIncrement}
-          onDecrement={this.onDecrement}
-        />
-
-        <Euro amount={this.state.amount} />
-        <Pound amount={this.state.amount} />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Amount>
+    <Pound amount={amount} />
+    <Euro amount={amount} />
+  </Amount>
+);
 
 const Euro = ({ amount }) => <p>Euro: {amount * 0.86}</p>;
-
 const Pound = ({ amount }) => <p>Pound: {amount * 0.76}</p>;
-
-const Amount = ({ amount, onIncrement, onDecrement }) => (
-  <div>
-    <span>US Dollar: {amount} </span>
-
-    <button type="button" onClick={onIncrement}>
-      +
-    </button>
-    <button type="button" onClick={onDecrement}>
-      -
-    </button>
-  </div>
-);
 
 export default App;
